efa593c90d58de5668a9c0d49011402be0c3a5d3
user-forty-seven <userisfortyseven@gmail.com>
2025-02-25 19:18:40 +0530

    Added food particles

--- snake_game/code/main.cpp
+++ snake_game/code/main.cpp
@@ -1,15 +1,50 @@
 #include <raylib.h>
 
 
-int main(){
+int CELL_SIZE = 30;
+int CELL_NUMBER = 30;
+int WINDOW_HEIGHT = CELL_NUMBER*CELL_SIZE;
+int WINDOW_WIDTH = CELL_NUMBER*CELL_SIZE;
 
-    InitWindow(300,300,"Window");
+
+class Food{
+
+    public:
+        Food(){
+            position = generate_random_position();
+        }
+
+        Vector2 position;
+
+
+
+        //methods
+        Vector2 generate_random_position(){
+            int x = GetRandomValue(0,CELL_NUMBER);
+            int y = GetRandomValue(0, CELL_NUMBER);
+        
+            return {x,y};
+        }
+
+        void draw_food(){
+            DrawRectangle(position.x*CELL_NUMBER, position.y*CELL_NUMBER, CELL_SIZE, CELL_SIZE, RED);
+        }
+
+};
+
+
+int main(){
+    
+    InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"Snake game");
     SetTargetFPS(60);
+
+    Food food;
 
 
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(BLACK);
+        food.draw_food();
         EndDrawing();
 
     }
@@ -17,3 +52,5 @@ int main(){
     CloseWindow();
 
 }
+
+//g++ -o main code/main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
