2b20e9d4525c1a7b384bf59bb4ab1e8eed44c35d
user-forty-seven <notlucky750@gmail.com>
2025-02-27 04:10:17 +0530

    snake eats food and grows longer now
    added the logic for the snake to eat food and grow longer
    
    also, added the include and lib header files to enable auto completions in nvmi :)

--- snake_game/code/main.cpp
+++ snake_game/code/main.cpp
@@ -1,119 +1,133 @@
-#include <raylib.h>
 #include <deque> //for the snake body
-#include <raymath.h>
 #include <iostream>
+#include <raylib.h>
+#include <raymath.h>
 
 int CELL_SIZE = 30;
 int CELL_NUMBER = 30;
-int WINDOW_HEIGHT = CELL_NUMBER*CELL_SIZE;
-int WINDOW_WIDTH = CELL_NUMBER*CELL_SIZE;
+int WINDOW_HEIGHT = CELL_NUMBER * CELL_SIZE;
+int WINDOW_WIDTH = CELL_NUMBER * CELL_SIZE;
 
-float initial_time  = 0;
+float initial_time = 0;
 
-bool interval(float time_interval){
-    float current_time = GetTime();
-    if(current_time-initial_time > time_interval){
-        initial_time = current_time;
-        return true;
+bool interval(float time_interval) {
+  float current_time = GetTime();
+
+  if (current_time - initial_time > time_interval) {
+    initial_time = current_time;
+    return true;
+  }
+  return false;
+}
+
+class Food {
+
+public:
+  Food() { position = generate_random_position(); }
+
+  Vector2 position;
+
+  // methods
+  Vector2 generate_random_position() {
+    float x = GetRandomValue(0, CELL_NUMBER - 1);
+    float y = GetRandomValue(0, CELL_NUMBER - 1);
+    return {x, y};
+  }
+
+  void draw_food() {
+    DrawRectangle(position.x * CELL_NUMBER, position.y * CELL_NUMBER, CELL_SIZE,
+                  CELL_SIZE, RED);
+  }
+};
+
+class Snake {
+
+public:
+  std::deque<Vector2> body = {Vector2{4, 7}, Vector2{5, 7}, Vector2{6, 7}};
+  Vector2 direction = {1, 0};
+
+  // methods
+  void draw_body() {
+    for (unsigned int i = 0; i < body.size(); i++) {
+      // DrawRectangle(body[i].x*CELL_NUMBER, body[i].y*CELL_NUMBER, CELL_SIZE,
+      // CELL_SIZE, YELLOW);
+      DrawRectangleRounded({body[i].x * CELL_NUMBER, body[i].y * CELL_NUMBER,
+                            (float)CELL_SIZE, (float)CELL_SIZE},
+                           0.5, 1, BLUE);
+    }
+  }
+
+  void update_state() {
+    body.pop_back();
+    body.push_front(Vector2Add(direction, body[0]));
+  }
+  void add_segments(Vector2 food_position){
+      body.push_front(Vector2Add(direction, body[0]));
+  }
+
+  bool check_collision_food(Vector2 food_position){
+    if(Vector2Equals(food_position, body[0])){
+      return true;
     }
     return false;
-}
+  }
 
-
-class Food{
-
-    public:
-        Food(){
-            position = generate_random_position();
-        }
-
-        Vector2 position;
-
-        //methods
-        Vector2 generate_random_position(){
-            float x = GetRandomValue(0,CELL_NUMBER-1);
-            float y = GetRandomValue(0, CELL_NUMBER-1);
-            return {x, y};
-        }
-
-        void draw_food(){
-            DrawRectangle(position.x*CELL_NUMBER, position.y*CELL_NUMBER, CELL_SIZE, CELL_SIZE, RED);
-            
-        }
 
 };
 
-
-class Snake{
-
-    public:
-        std::deque<Vector2> body = {Vector2{4,7}, Vector2{5,7}, Vector2{6,7}};
-        Vector2 direction = {1,0};
-
-        
-        //methods
-        void draw_body(){
-            for(unsigned int i = 0; i<body.size(); i++){
-                //DrawRectangle(body[i].x*CELL_NUMBER, body[i].y*CELL_NUMBER, CELL_SIZE, CELL_SIZE, YELLOW);
-                DrawRectangleRounded({body[i].x*CELL_NUMBER, body[i].y*CELL_NUMBER, (float) CELL_SIZE, (float) CELL_SIZE}, 0.5, 1, BLUE);
-            }
-        }
-
-        void update_state(){
-                body.pop_back();
-                body.push_front(Vector2Add(direction,body[0]));
-        }
-};
-
-
-class Game{
-    public:
-        bool check_collision_wall(Vector2 snake_head){
-            if(snake_head.x < 0 || snake_head.x>30 || snake_head.y < 0 || snake_head.y > 30){
-                return true;
-            }
-            return false;
-        }
-
-};
-
-int main(){
-    
-    InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"Snake game");
-    SetTargetFPS(60);
-
-    Food food;
-    Snake snake;
-    Game game;
-
-    while (!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground(BLACK);
-        food.draw_food();
-        snake.draw_body();
-        if(IsKeyPressed(KEY_UP) && snake.direction.y != 1){
-            snake.direction = {0,-1};
-        }
-        if(IsKeyPressed(KEY_DOWN) && snake.direction.y != -1){
-            snake.direction = {0,1};
-        }
-        if(IsKeyPressed(KEY_LEFT) && snake.direction.x != 1){
-            snake.direction = {-1,0};
-        }
-        if(IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1){
-            snake.direction = {1,0};
-        }
-        
-        if (interval(0.2)){snake.update_state();}
-        if(game.check_collision_wall(snake.body[0])){
-            std::cout << "Wall collision: {" << snake.body[0].x << "," << snake.body[0].y << "}" << std::endl;
-        }
-        EndDrawing();
-
+class Game {
+public:
+  bool check_collision_wall(Vector2 snake_head) {
+    if (snake_head.x < 0 || snake_head.x > 30 || snake_head.y < 0 ||
+        snake_head.y > 30) {
+      return true;
     }
-    
-    CloseWindow();
+    return false;
+  }
+};
 
+int main() {
+
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Snake game");
+  SetTargetFPS(60);
+
+  Food food;
+  Snake snake;
+  Game game;
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    food.draw_food();
+    snake.draw_body();
+    if (IsKeyPressed(KEY_UP) && snake.direction.y != 1) {
+      snake.direction = {0, -1};
+    }
+    if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1) {
+      snake.direction = {0, 1};
+    }
+    if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1) {
+      snake.direction = {-1, 0};
+    }
+    if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1) {
+      snake.direction = {1, 0};
+    }
+
+    if (interval(0.2)) {
+      snake.update_state();
+    }
+    if (game.check_collision_wall(snake.body[0])) {
+      std::cout << "Wall collision: {" << snake.body[0].x << ","
+                << snake.body[0].y << "}" << std::endl;
+    }
+    if(snake.check_collision_food(food.position)){
+      snake.add_segments(food.position);
+      food.position = food.generate_random_position();
+    }
+    EndDrawing();
+  }
+
+  CloseWindow();
 }
 
-//g++ -o main code/main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+// g++ -o main code/main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
