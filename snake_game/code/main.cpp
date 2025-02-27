a4c00bb7b46053f113524232def36dd987670837
user-forty-seven <notlucky750@gmail.com>
2025-02-27 22:24:03 +0530

    added logic to esure that the food doesnt spawn in the snake body

--- snake_game/code/main.cpp
+++ snake_game/code/main.cpp
@@ -42,10 +42,20 @@ public:
 
 
   // methods
-  Vector2 generate_random_position() {
-    float x = GetRandomValue(0, CELL_NUMBER - 1);
-    float y = GetRandomValue(0, CELL_NUMBER - 1);
-    return {x, y};
+  Vector2 generate_random_coordinates(){
+    float x = GetRandomValue(0, CELL_NUMBER-1);
+    float y = GetRandomValue(0, CELL_NUMBER-1);
+    return {x,y};
+  }
+  Vector2 generate_random_position(std::deque<Vector2> snake_body = {Vector2{0,0}}) {
+    Vector2 position = generate_random_coordinates();
+    for(unsigned int i = 0; i<snake_body.size(); i++){  //this logic ensures that the food particle doesnt spawn in the snake body's coordinates
+      if(Vector2Equals(snake_body[i], position)){ 
+
+          position = generate_random_coordinates();
+      }
+    }
+    return position;
   }
 
   void draw_food() {
@@ -132,16 +142,16 @@ int main() {
     ClearBackground(BLACK);
     food.draw_food();
     snake.draw_body();
-    if (IsKeyPressed(KEY_UP) && snake.direction.y != 1) {
+    if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W) && snake.direction.y != 1) {
       snake.direction = {0, -1};
     }
-    if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1) {
+    if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S) && snake.direction.y != -1) {
       snake.direction = {0, 1};
     }
-    if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1) {
+    if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A) && snake.direction.x != 1) {
       snake.direction = {-1, 0};
     }
-    if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1) {
+    if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D) && snake.direction.x != -1) {
       snake.direction = {1, 0};
     }
 
@@ -154,7 +164,7 @@ int main() {
     }
     if(snake.check_collision_food(food.position)){
       snake.add_segments(food.position);
-      food.position = food.generate_random_position();
+      food.position = food.generate_random_position(snake.body);
     }
     EndDrawing();
   }
